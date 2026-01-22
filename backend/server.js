22b98964bc429ea5fec64e9728a4681b633501d6
alexi <iorgaalexandru22@stud.ase.ro>
2026-01-22 20:04:54 +0200

    reinstalled vite

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Server is running!');
+});
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
