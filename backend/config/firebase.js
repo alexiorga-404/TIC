c7f543947653ee306b2d759ede072ea934c51242
alexi <iorgaalexandru22@stud.ase.ro>
2026-01-26 17:08:50 +0200

    configure .env for backend and added firebase connection data

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin')
-const serviceAccount = require('./serviceAccountKey.json')
-
+//const serviceAccount = require('./serviceAccountKey.json')
+const dotenv = require('dotenv');
 dotenv.config();
 
 const serviceAccount = {
